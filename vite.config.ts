e314d84aef1003a763797764b4666001c6f71b02
Abdurrahman <bootcampelfan@gmail.com>
2026-07-03 20:30:06 -0700

    merubah file vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,30 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+
+export default defineConfig({
+  vite: {
+    server: {
+      host: true,
+      allowedHosts: true,
+    },
+
+
+    ssr: {
+      noExternal: [
+        "@tanstack/start",
+        "@tanstack/start-server-core",
+        "@tanstack/start-client-core",
+      ],
     },
   },
-}));
+
+
+  tanstackStart: {
+    server: {
+      entry: "server",
+    },
+  },
+
+
+  nitro: true,
+});
